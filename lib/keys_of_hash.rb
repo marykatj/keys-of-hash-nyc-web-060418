7481ef5b55a4a4a2a6684670a323456f8dec4fc4
Mary-Kate James <marykatj@gmail.com>
2018-05-23 23:28:30 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+array = []
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments, value)
+    arguments.each do |key, values|
+      if value == values
+        array.push(key)
+      end
+    end
   end
+  array
 end
